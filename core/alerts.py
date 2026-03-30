873fbcea1427ca87910175753436d14a3b684afd
Haneee20 <haneefa.syednadeem@gmail.com>
2026-03-30 21:42:41 +0530

    Day 3: added alert formatting function

--- core/alerts.py
+++ core/alerts.py
@@ -5,3 +5,12 @@ def generate_alert(score):
         return "MEDIUM RISK"
     else:
         return "LOW RISK"
+
+def format_alert(device_mac, score, reasons):
+    risk = generate_alert(score)
+
+    return {
+        "device_mac": device_mac,
+        "risk": risk,
+        "reasons": reasons
+    }
