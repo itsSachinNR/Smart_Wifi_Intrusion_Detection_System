d8580910f0444c11d2b42d5b2d4a9a1183146097
SACHIN NR <itssachinnr@gmail.com>
2026-04-22 21:39:50 +0530

    System 1 : Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
-
 from flask import Flask, render_template
 from core.detector import process_events
-from core.alerts import generate_alert
 
 app = Flask(__name__)
+
 
 @app.route("/")
 def home():
     return render_template("index.html")
+
 
 @app.route("/analyze")
 def analyze():
@@ -16,16 +16,15 @@ def analyze():
     final_results = []
 
     for item in results:
-        risk = generate_alert(item["score"])
-
         final_results.append({
             "device_mac": item["device_mac"],
             "score": item["score"],
-            "risk": risk,
-            "reason": item["event_type"]
+            "risk": item["risk"],       
+            "reasons": item["reasons"]  
         })
 
     return render_template("result.html", results=final_results)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
